c2f3e453c9f932f32f34de36e9214a33d050e2f3
Heli Hård Källoff <vierge.hard@gmail.com>
2024-02-23 12:54:56 +0100

    better presentation of sort alternatives

--- sort-btn.js
+++ sort-btn.js
@@ -1,9 +1,9 @@
 export const container = document.querySelector("#blanket");
 const sortBtn = document.querySelector("#sort-btn");
-sortBtn.innerText = "nyast först";
+sortBtn.innerText = "äldst först";
 
 sortBtn.addEventListener("click", function () {
   container.classList.toggle("reverse");
-  sortBtn.innerText = "äldst först";
-  container.classList.contains("reverse") ? sortBtn.innerText = "äldst först" : sortBtn.innerText = "nyast först";
+  sortBtn.innerText = "nyast först";
+  container.classList.contains("reverse") ? sortBtn.innerText = "nyast först" : sortBtn.innerText = "äldst först";
 });
